622968a76d5bf55d0a58489bcc21b4e9a95c9f52
leif <leiflyy@outlook.com>
2018-02-01 22:13:20 +0800

    使用 element.tabAdd 动态加载页面

---
+++ application/admin/controller/LogController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author: EricLi <leiflyy@outlook.com>
+ */
+
+namespace app\admin\controller;
+
+
+use controller\BaseController;
+
+class LogController extends BaseController
+{
+
+}
